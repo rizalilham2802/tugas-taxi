95f635aaf8132ad6eb190bd3991620aeef78cf5b
mukhamad rizal <mukhamad.ilham@students.amikom.ac.id>
2020-03-21 11:52:02 +0700

    program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objek taxi
+            Taxi taxi = new Taxi();
+
+            //nilai properties
+            taxi.Drivername = "rizal";
+            taxi.OnDuty = true;
+            taxi.Numpassenger = 12;
+
+            //panggilan method
+            taxi.TaxiInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    public class Taxi
+    {
+        //properties
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public float Numpassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drivername: {0}", Drivername);
+            Console.WriteLine("OnDuty: {0}", OnDuty);
+            Console.WriteLine("Numpassenger: {0}", Numpassenger);
+
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang ", Drivername);
+
+        }
+        public void DropOffpassenger()
+        {
+            Console.WriteLine("{0} sedang antar penumpang ", Drivername);
+
+        }
+    }
+}
